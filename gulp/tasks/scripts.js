a8ca4194c2ab14678f4363012baa4904502e654c
Ricardo Pabón <rpabon257@gmail.com>
2015-05-31 20:33:53 +0200

    Add source maps to browserify modules

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp');
 var paths = require('../config').paths;
 var flag = require('yargs').argv;
+var runSequence = require('run-sequence');
+var eventStream = require('event-stream');
+var sourcemaps = require('gulp-sourcemaps');
 var _if = require('gulp-if');
+var connect = require('gulp-connect');
 var jshint = require('gulp-jshint');
 var hintStylish = require('jshint-stylish');
-var browserify = require('gulp-browserify');
-var uglify = require('gulp-uglify');
-var runSequence = require('run-sequence');
-var connect = require('gulp-connect');
-var doNothing = function() {};
 var jscs = require('gulp-jscs');
 var codeStylish = require('gulp-jscs-stylish');
-var eventStream = require('event-stream');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+var doNothing = function() {};
 
 gulp.task('scripts:hint', function() {
   var taskStream = gulp.src(paths.tasks);
@@ -27,6 +28,8 @@ gulp.task('scripts:hint', function() {
 gulp.task('scripts:bundle', function() {
   return gulp.src(paths.js.src)
     .pipe(browserify({insertGlobals: true, debug: !flag.production}))
+    .pipe(_if(!flag.production, sourcemaps.init({loadMaps: true})))
+    .pipe(_if(!flag.production, sourcemaps.write()))
     .pipe(_if(flag.production, uglify()))
     .pipe(gulp.dest(paths.js.dest))
     .pipe(connect.reload());
